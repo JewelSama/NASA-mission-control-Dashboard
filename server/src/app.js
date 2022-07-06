3930c4c34e663b45ab0c1bb37bc2c873f5d76383
Jewel Osafile <osafilejewel@gmail.com>
2022-07-06 19:59:41 +0100

    run deploy

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const planetsRouter = require('./routes/planets/planets.router')
 const cors = require('cors')
 
@@ -7,9 +8,17 @@ const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
-}))
+}));
+
+
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 
 module.exports = app;
